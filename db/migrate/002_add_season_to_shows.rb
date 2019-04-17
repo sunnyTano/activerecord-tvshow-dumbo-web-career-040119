14a3157d99f27f8dd4abd3e8114423281a696529
Susan Tan <ssusantan9@gmail.com>
2019-04-17 05:25:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,15 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
+<<<<<<< HEAD
     def change
       add_column :shows, :season, :string
     end 
   
   end 
+=======
+  def change
+    add_column :shows, :season, :string
+  end 
+
+end 
+>>>>>>> e9dbf2df63a95c5e5d28d47dfad6aa9ee54e8abc
